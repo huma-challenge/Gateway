485d40ce50da286916252ba901ba9ddc3afad95b
bigsbug <booxgitboox@gmail.com>
2022-08-15 15:02:22 -0400

    fix permission checker of RequestHaveToken

--- gateway/account_manager/permissions.py
+++ gateway/account_manager/permissions.py
@@ -5,5 +5,6 @@ from django.http.request import HttpRequest
 class RequestHaveToken(permissions.BasePermission):
     message = "The Request Header Does Not Have Token"
 
-    def has_object_permission(self, request: HttpRequest, view, obj):
+    def has_permission(self, request: HttpRequest, view):
+        return False
         return hasattr(request, "token")
