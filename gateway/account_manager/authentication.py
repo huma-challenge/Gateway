2fb31e538606e7296fe68e56f377514a95914374
bigsbug <booxgitboox@gmail.com>
2022-08-15 14:37:10 -0400

    create new authentication class for UserToken

---
+++ gateway/account_manager/authentication.py
@@ -0,0 +1,39 @@
+from google.protobuf.json_format import ParseDict
+
+# These protocol buffers are generated automatically
+# for viewing protocol buffers go to the Protobuffs repository
+# https://github.com/huma-challenge/Protobuffs/tree/main/account_manager
+from protobufs.account_manager import account_pb2 as acc_typ
+
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import exceptions
+
+
+def get_authorization_header(request):
+    """
+    Return request's 'HTTP_USERTOKEN_AUTHORIZATION:' header, as a bytestring.
+    Hide some test client ickyness where the header can be unicode.
+    """
+    auth = request.META.get("HTTP_AUTHORIZATION", "")
+    return auth
+
+
+class UserTokenAuthentication(TokenAuthentication):
+    """DRF TokenAuthentication that uses HTTP_USERTOKEN_AUTHORIZATION header."""
+
+    def authenticate(self, request):
+        auth = get_authorization_header(request)
+
+        if not auth:
+            msg = "Invalid token header. No credentials provided."
+            raise exceptions.AuthenticationFailed(msg)
+        if isinstance(auth, bytes):
+            auth = auth.decode("utf-8")
+        else:
+            ...
+        token = auth
+        user_token = ParseDict({"token": str(token)}, acc_typ.UserToken())
+        request.token = user_token
+
+        return (user_token, user_token)
