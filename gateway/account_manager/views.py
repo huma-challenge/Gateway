1c2b5fa446dd74457c8aab54f91d38ae6efe9bb5
bigsbug <booxgitboox@gmail.com>
2022-08-15 11:33:37 -0400

    create list rest-api for account-manager app

--- gateway/account_manager/views.py
+++ gateway/account_manager/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from google.protobuf.json_format import MessageToDict, ParseDict
 
-# Create your views here.
+# These protocol buffers are generated automatically
+# for viewing protocol buffers go to the Protobuffs repository
+# https://github.com/huma-challenge/Protobuffs/tree/main/account_manager
+from protobufs.account_manager import account_pb2 as acc_typ
+from protobufs.account_manager import account_pb2_grpc as acc_grpc
+
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
+from gateway.account_manager.grcp_app import get_grcp_manager
+
+# Generate A gRPC manager based on the app
+grcp_manager = get_grcp_manager()
+
+# generate client and register on the app channel
+def generate_user_manager_stub() -> acc_grpc.UserManagerStub:
+    return grcp_manager.create_new_client(acc_grpc.UserManagerStub)
+
+
+# Create Token for testing in development mode
+request_login = acc_typ.UserLoginRequest()
+request_login.username = "Ali"
+request_login.password = "nova-man"
+token = generate_user_manager_stub().Login(request_login)
+
+
+class AccountManager(ViewSet):
+    def list(self, request):
+        client = generate_user_manager_stub()
+
+        # Generate RPC Request
+        request_list = acc_typ.UserListRequest()
+        request_list.token.CopyFrom(token.token)
+
+        # fetch all users and convert from message to a list of dict
+        user_list = [MessageToDict(user, True) for user in client.List(request_list)]
+
+        return Response(user_list, status.HTTP_200_OK)
