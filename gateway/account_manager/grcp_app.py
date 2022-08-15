ddc7ee090f453c7d3a328eedd48ef9d2ae77055e
bigsbug <booxgitboox@gmail.com>
2022-08-15 18:41:24 -0400

    load target address from env

--- gateway/account_manager/grcp_app.py
+++ gateway/account_manager/grcp_app.py
@@ -1,7 +1,12 @@
 from gateway.grpc_manager.manager import Manager
 
+import environ
+
+# Define and loads env var files
+env = environ.Env()
+
 # The address of gRPC Server on the network
-target = "127.0.0.1:50051"
+target = env("account_manager", default="127.0.0.1:50051")
 
 
 def get_grcp_manager():
