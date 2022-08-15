a194cabc32c67a5d9bac4a6592c71a5bb9db6f2f
bigsbug <booxgitboox@gmail.com>
2022-08-15 15:25:07 -0400

    improve the RequestHaveToken permission class of account-manager

--- gateway/account_manager/permissions.py
+++ gateway/account_manager/permissions.py
@@ -1,10 +1,16 @@
 from rest_framework import permissions
 from django.http.request import HttpRequest
 
+# These protocol buffers are generated automatically
+# for viewing protocol buffers go to the Protobuffs repository
+# https://github.com/huma-challenge/Protobuffs/tree/main/account_manager
+from protobufs.account_manager import account_pb2 as acc_typ
+
 
 class RequestHaveToken(permissions.BasePermission):
     message = "The Request Header Does Not Have Token"
 
     def has_permission(self, request: HttpRequest, view):
+        if hasattr(request, "token"):
+            return isinstance(request.token, acc_typ.UserToken)
         return False
-        return hasattr(request, "token")
